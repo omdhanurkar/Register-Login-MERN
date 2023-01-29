8dd0a0468b9a4171ae25037c5a90eaf50c0635f9
Omprakash <omdhanurkar444@gmail.com>
2023-01-30 00:24:01 +0530

    routers

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser,loginUser } = require("../controllers/userController")
-
+const { registerUser, loginUser,forget_password ,reset_password } = require("../controllers/userController");
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
+router.post("/Register", registerUser)
+router.post("/Login", loginUser)
 
-router.post("/register", registerUser)
-router.post("/login", loginUser)
+router.post("/api/forget-password" ,forget_password);
 
+router.post("/api/reset-password" ,reset_password);
 
 
 module.exports = router
